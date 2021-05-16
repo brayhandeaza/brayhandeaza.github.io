eb15fbcfb1bfca03091dd2c9441cab1b18ed90be
Brayhan De Aza <brayhandeaza@gmail.com>
2021-05-16 17:16:45 -0400

    Updates

---
+++ precache-manifest.9b2b4a008ea3f89a94bd382e0aededce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9979b56a1c540293970d",
+    "url": "/static/css/main.9e17a727.chunk.css"
+  },
+  {
+    "revision": "9979b56a1c540293970d",
+    "url": "/static/js/main.04a494a0.chunk.js"
+  },
+  {
+    "revision": "018a923b25668aaafd87",
+    "url": "/static/js/runtime~main.ffba608c.js"
+  },
+  {
+    "revision": "9e49c4a8650d28a005ae",
+    "url": "/static/js/2.dc79434e.chunk.js"
+  },
+  {
+    "revision": "ffc47a6560474014328a",
+    "url": "/static/js/3.a04b6fff.chunk.js"
+  },
+  {
+    "revision": "4ea88ac4f3e0089574a912923409ff75",
+    "url": "/static/media/tick-mark.4ea88ac4.svg"
+  },
+  {
+    "revision": "103e42c235fca12d922d6c23bcc29f45",
+    "url": "/index.html"
+  }
+];
