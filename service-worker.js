f8585bf0dabf10ab9344534a5a52238ec8058c7b
Brayhan De Aza <brayhandeaza@gmail.com>
2021-05-19 13:32:34 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2a152ca020fb6af5d95fa4acc27c792.js"
+  "/precache-manifest.a4b12531f515e49e5f91f190415bc8e4.js"
 );
 
 workbox.clientsClaim();
