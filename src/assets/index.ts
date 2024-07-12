94863a5ecf2bd14b641efd78378f99d6f24460aa
Brayhan De Aza <brayhandeaza@gmail.com>
2024-07-11 21:51:04 -0400

    new rm

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,7 +3,7 @@ import HomeIllustration from './home-illustration.png';
 import github from './github.png';
 import notion from './notion.png';
 
-const resume = 'https://res.cloudinary.com/brayhandeaza/image/upload/v1720744096/Brayhan/fwc8qazc3ldcpdgnxo4k.pdf';
+const resume = 'https://res.cloudinary.com/brayhandeaza/image/upload/v1720748980/Brayhan/eauhynttx6og1panelbk.pdf';
 
 
 export {
